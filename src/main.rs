1ef887b239562e597581fa1aa8aa9850d824eb66
Floris <kappenfloris@gmail.com>
2024-04-24 15:56:00 +0200

    fix: make relevant modules public

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-mod structs;
-mod abstraction {
+pub mod structs;
+pub mod abstraction {
     pub mod action_abstraction;
 }
-mod game_states {
-    mod kuhn_poker {
+pub mod game_states {
+    pub mod kuhn_poker {
         pub mod game_state;
     }
-    mod leduc_poker {
+    pub mod leduc_poker {
         pub mod game_state;
     }
     pub mod nlth_poker {
@@ -16,8 +16,8 @@ mod game_states {
     pub mod base_game_state;
 }
 mod tests {
-    pub mod nlth_headsup;
-    pub mod nlth_multiplayer;
+    mod nlth_headsup;
+    mod nlth_multiplayer;
 }
 
 fn main() {
