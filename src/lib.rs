eb8e61e03af6e8985c3aae4a03d59a2a7989f8c1
Floris <kappenfloris@gmail.com>
2024-04-24 15:39:34 +0200

    feature: make it a library

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+mod structs;
+mod abstraction {
+    pub mod action_abstraction;
+}
+mod game_states {
+    pub mod kuhn_poker {
+        pub mod game_state;
+    }
+    pub mod leduc_poker {
+        pub mod game_state;
+    }
+    pub mod nlth_poker {
+        pub mod game_state;
+        pub mod rank;
+    }
+    pub mod base_game_state;
+}
